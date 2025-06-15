59ae9df528bfc6728848d3ea05caf059e51bfd16
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-06-15 14:27:19 +0700

    init project

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { HeroUIProvider, ToastProvider } from "@heroui/react";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <HeroUIProvider>
+      <ToastProvider placement="top-center" />
+      <App />
+    </HeroUIProvider>
+  </StrictMode>,
+);
