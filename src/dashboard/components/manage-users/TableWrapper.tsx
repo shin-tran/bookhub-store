43c7429400a3646adc6ade5e5964cf5d81b71e1d
ngocshintrannn <ngoc.shintrann@gmail.com>
2025-06-20 10:38:33 +0700

    feat: add paginated

--- src/dashboard/components/manage-users/TableWrapper.tsx
+++ src/dashboard/components/manage-users/TableWrapper.tsx
@@ -1,4 +1,6 @@
 import {
+  Pagination,
+  Spinner,
   Table,
   TableBody,
   TableCell,
@@ -13,25 +15,50 @@ import { useState } from "react";
 
 export const TableWrapper = () => {
   const [page, setPage] = useState(1);
-  const pageSize = 20;
-  const { data: users } = useGetPaginations(page, pageSize);
-  console.log(users?.result);
+  const [pageSize, setPageSize] = useState(10);
+  const { data: users, isLoading } = useGetPaginations(page, pageSize);
+  const loadingState =
+    isLoading || users?.meta.total === 0 ? "loading" : "idle";
+
+  console.log(users);
   return (
     <div className="flex w-full flex-col gap-4">
-      <Table aria-label="Example table with custom cells">
+      <Table
+        aria-label="Manage Users"
+        bottomContent={
+          <div className="flex w-full justify-center">
+            <Pagination
+              isCompact
+              showControls
+              showShadow
+              color="secondary"
+              page={page}
+              total={users?.meta.pages || 1}
+              onChange={(page) => setPage(page)}
+            />
+          </div>
+        }
+      >
         <TableHeader columns={[...STATIC_CONSTANTS.COLUMNS]}>
           {(column) => (
             <TableColumn
               key={column.uid}
               align={
-                ["actions", "isActive", "role"].includes(column.uid) ? "center" : "start"
+                ["actions", "isActive", "role"].includes(column.uid)
+                  ? "center"
+                  : "start"
               }
             >
               {column.name}
             </TableColumn>
           )}
         </TableHeader>
-        <TableBody items={users?.result || []}>
+        <TableBody
+          items={users?.result || []}
+          loadingContent={<Spinner />}
+          isLoading={isLoading}
+          loadingState={loadingState}
+        >
           {(item) => (
             <TableRow key={item._id}>
               {(columnKey) => (
